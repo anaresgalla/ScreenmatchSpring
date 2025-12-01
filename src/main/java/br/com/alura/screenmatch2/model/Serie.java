a6aa5ccf07ea13050b7c6884ca32759d4ceff3d0
anaresgalla <anaresgalla@gmail.com>
2025-12-01 15:44:39 -0300

    Getters/Setters/ToString de Serie

--- src/main/java/br/com/alura/screenmatch2/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch2/model/Serie.java
@@ -24,4 +24,80 @@ public class Serie {
         this.sinopse = dadosSerie.sinopse();
         this.poster = dadosSerie.poster();
     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public void setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getElenco() {
+        return elenco;
+    }
+
+    public void setElenco(String elenco) {
+        this.elenco = elenco;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    @Override
+    public String toString() {
+        return "genero = " + genero +
+                ", titulo = " + titulo +
+                ", totalTemporadas = " + totalTemporadas +
+                ", avaliacao = " + avaliacao +
+                ", sinopse= '" + sinopse + "'" +
+                ", ano = " + ano +
+                ", elenco = '" + elenco + "'" +
+                ", poster = '" + poster + "'";
+    }
 }
