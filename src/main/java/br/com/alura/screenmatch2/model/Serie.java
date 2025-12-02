7ccd5c850b14b4a205f361805fb345f36312fa7a
anaresgalla <anaresgalla@gmail.com>
2025-12-02 15:43:18 -0300

    Cria tabela no PostgreSQL

--- src/main/java/br/com/alura/screenmatch2/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch2/model/Serie.java
@@ -1,13 +1,22 @@
 package br.com.alura.screenmatch2.model;
 
 import br.com.alura.screenmatch2.service.traducao.ConsultaMyMemory;
+import jakarta.persistence.*;
 
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String titulo;
     private Integer totalTemporadas;
     private Double avaliacao;
+    @Enumerated(EnumType.STRING)
     private Genero genero;
     private String sinopse;
     private String ano;
@@ -25,6 +34,14 @@ public class Serie {
         this.elenco = dadosSerie.elenco();
         this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse()).trim();
         this.poster = dadosSerie.poster();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
